4a4356ebe57d49c77b9cf8f2484aaf16955a06de
Hawyar <hawyarxoshnaw@gmail.com>
2020-04-26 22:45:21 -0400

    finished caos html elements

--- src/app.js
+++ src/app.js
@@ -14,3 +14,31 @@ closeBtn.addEventListener("click", function () {
 // caos table content
 
 // fetch data and inject into list
+
+//create courses modal
+//content
+var modalBtnCcg = document.querySelector("#create-ccg-button");
+var modalBgCcg = document.querySelector(".modal-ccg");
+var closeBtnCcg = document.querySelector(".close-modal-ccg");
+
+modalBtnCcg.addEventListener("click", function () {
+  modalBgCcg.classList.add("bg-active");
+});
+
+closeBtnCcg.addEventListener("click", function () {
+  modalBgCcg.classList.remove("bg-active");
+});
+
+// create student group modal
+//content
+var modalBtnCsg = document.querySelector("#create-Student-button");
+var modalBgCsg = document.querySelector(".modal-csg");
+var closeBtnCsg = document.querySelector(".close-modal-csg");
+
+modalBtnCsg.addEventListener("click", function () {
+  modalBgCsg.classList.add("bg-active");
+});
+
+closeBtnCsg.addEventListener("click", function () {
+  modalBgCsg.classList.remove("bg-active");
+});
